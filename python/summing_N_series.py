7bd4bf9c744292b77034afe61e42de02f8f20a6f
alex <alexandra.stancu21@gmail.com>
2018-02-15 23:11:11 +0200

    Initial commit: Solved Summing N Series

---
+++ python/summing_N_series.py
@@ -0,0 +1,23 @@
+# HackerRank
+# Summing the N series
+
+DIVISOR = 10**9+7
+
+def get_user_input():
+    T = int(input())
+
+    inputs = []
+    for i in range(T):
+        inputs.append(int(input()))
+    return inputs
+
+def modulo(dividend, divisor):
+    return ((dividend % divisor)*dividend) % divisor
+
+def expression():
+    inputs = get_user_input()
+
+    for no in inputs:
+        print(modulo(no, DIVISOR))
+
+expression()
